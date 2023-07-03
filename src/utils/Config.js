8e9686f819ffeae594253d08588e79854e4a5f7f
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2023-07-02 20:20:42 -0400

    add additional variables to config file

--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,3 +1,6 @@
 export class Config {
-     FILE_NAME = 'generated-poster.png';
+    static API_URL = 'https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=';
+    static DEFAULT_ARTIST = 'Tame Impala';
+    static DEFAULT_ALBUM = 'Currents';
+    static FILE_NAME = 'generated-poster.png';
 }
