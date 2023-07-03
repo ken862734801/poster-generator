4805f9b89b4faf3d27d7d005ad28b6a3ea604f44
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2023-07-02 21:15:27 -0400

    add additional var to config file, update header component to use config var

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,7 +3,7 @@ import { Config } from "../../utils/Config.js";
 import DomToImage from "dom-to-image";
 
 function saveAsPng(name){
-    DomToImage.toPng(document.getElementById("poster-container"), {quality:1})
+    DomToImage.toPng(document.getElementById("poster-container"), { quality:1 })
         .then(function(dataUrl){
             let link = document.createElement("a");
             link.download = name;
@@ -16,7 +16,7 @@ export default function Header(){
     return(
         <header>
             <div className="header-container">
-                <a href=""><h1>Poster Generator</h1></a>
+                <a href=""><h1>{Config.APP_NAME}</h1></a>
                 <button onClick={()=> saveAsPng(Config.FILE_NAME)}>DOWNLOAD</button>
             </div>
         </header>
--- src/utils/Config.js
+++ src/utils/Config.js
@@ -1,5 +1,6 @@
 export class Config {
     static API_URL = 'https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=';
+    static APP_NAME = 'Poster Generator';
     static DEFAULT_ALBUM = 'Currents';
     static DEFAULT_ARTIST = 'Tame Impala';
     static DEFAULT_BACKGROUND_COLOR = '#000000';
