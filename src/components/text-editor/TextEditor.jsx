447202f943ecff2448b052853de9700ea3909fd0
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2023-12-03 20:59:43 -0500

    task: update styling on text editor

--- src/components/text-editor/TextEditor.jsx
+++ src/components/text-editor/TextEditor.jsx
@@ -6,20 +6,53 @@ const fields = ['year', 'album', 'artist', 'date', 'duration', 'label'];
 function TextEditor(props){
     const { poster, setPoster } = props;
 
-    function renderTextInputFields(arr){
-        return arr.map((field) => (
-            <TextInput
-                key={field}
-                inputName={field}
-                inputValue={poster.data[field]}
-                poster={poster}
-                setPoster={setPoster}
-            />
-        ))
-    }
+    // function renderTextInputFields(arr){
+    //     return arr.map((field) => (
+    //         <TextInput
+    //             key={field}
+    //             inputName={field}
+    //             inputValue={poster.data[field]}
+    //             poster={poster}
+    //             setPoster={setPoster}
+    //         />
+    //     ))
+    // }
+    // function renderGenreTextInputFields(){
+    //     return (poster.data['genre']).map((data, index) => (
+    //         <TextInput key={index} inputName={`Genre #${index + 1}`} inputValue={data}/>       
+    //     ))
+    // }
+
+    function handleTextInputChange(e, key){
+        const { value } = e.target;
+
+        if(Array.isArray(poster['data'][key])){
+
+            console.log('This is an array!');
+        } else {
+            console.log('This is not an array!');
+        };
+    };
+
+
     return (
         <div>
-            {renderTextInputFields(fields)}
+            {fields.map((field) => (
+                <TextInput
+                    key={field}
+                    inputName={field}
+                    inputValue={poster['data'][field]}
+                    handleOnChange={(e) => handleTextInputChange(e, field)}
+                />
+            ))}
+            {(poster.data['genre']).map((data, index) => (
+                <TextInput
+                    key={`Genre + ${index + 1}`}
+                    inputName={`Genre # ${index + 1}`}
+                    inputValue={data}
+                    handleOnChange={(e) => handleTextInputChange(e, 'genre')}
+                />
+            ))}
         </div>
     )
 };
--- src/components/text-input/TextInput.jsx
+++ src/components/text-input/TextInput.jsx
@@ -1,19 +1,25 @@
+import './text-input.css';
 
 function TextInput (props){
-    const { inputName, inputValue, poster, setPoster } = props;
+    const { inputName, inputValue, poster, setPoster, handleOnChange } = props;
 
-    function handleTextInputChange(e){
-        const { value } = e.target;
-        const newPoster = {...poster};
-        newPoster.data[inputName] = value;
-        setPoster(newPoster)
-    };
+    // function handleTextInputChange(e){
+    //     const { value } = e.target;
+    //     const newPoster = {...poster};
+    //     newPoster.data[inputName] = value;
+    //     setPoster(newPoster)
+    // };
+    
 
     return (
         <>
-        <label htmlFor={inputName}>{inputName}</label>
+        <label className="text-input--label" htmlFor={inputName}>
+            <p>{inputName}</p>
+        </label>
         <div className='text-input'>
-            <input onChange={handleTextInputChange} type='text' name={inputName} value={inputValue}></input>
+            <div className='text-input--box'>
+                <input onChange={handleOnChange} type='text' name={inputName} value={inputValue}></input>
+            </div>
         </div>
         </>
     )
