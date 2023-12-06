b8e6a5fa258259041d6653a6786975c02c32e57d
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2023-12-05 21:37:33 -0500

    task: update styling on image editor

--- src/components/image-editor/ImageEditor.jsx
+++ src/components/image-editor/ImageEditor.jsx
@@ -1,13 +1,14 @@
 import './image-editor.css';
 import uploadImage from '../../assets/icons/upload-image.jpg';
+import { Trash } from '@phosphor-icons/react';
 import { useState } from 'react';
 
 function ImageEditor(){
-    const [gallery, setGallery] = useState([]);
+    const [images, setImages] = useState([uploadImage, uploadImage, uploadImage, uploadImage]);
 
     return (
         <div className='image-editor'>
-            { gallery.length === 0 ? (
+            { images.length === 0 ? (
                 <div className='image-gallery--empty'>
                     <div className='upload-image--container'>
                         <img src={ uploadImage }/>
@@ -15,7 +16,16 @@ function ImageEditor(){
                     </div>
                 </div>
             ) : (
-                <div className='image-gallery'></div>
+                <div className='image-gallery'>
+                    {images.map((image, index) => (
+                        <div className='image--container' key={ index }>
+                            <img src={image}/>
+                            <div className='trash-icon--container' title='Delete'>
+                                <Trash className='trash-icon' size={16}/>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             ) }
             <div className='image-editor-button--container'>
                 <input id='image-input' className='hidden' type='file'></input>
