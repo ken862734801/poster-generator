c3c6aadb6bb7224100dc79342a718e464eea439d
ken862734801 <kenshawmichaud@gmail.com>
2024-06-12 02:00:18 -0400

    env fix

--- src/configs.ts
+++ src/configs.ts
@@ -1,6 +1,8 @@
 export class Config {
     static URL =
         'https://ws.audioscrobbler.com/2.0/?method=album.getinfo&api_key=';
+    static DEV_API_KEY = 'a961cf7c34546fbbc73f9fc61ae62758';
+    static SHARED_SECRET = '5e60b0acbb05bf188fed0f5065c4c29e'
     static TITLE = 'Poster Generator';
     static DEFAULT_ARTIST = 'Tame Impala';
     static DEFAULT_ALBUM = 'Currents';
