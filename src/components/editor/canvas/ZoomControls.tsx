856325842881b327a84261127597b480cc21e9ff
ken862734801 <kenshawmichaud@gmail.com>
2025-05-30 22:25:42 -0400

    feat: zoom controls work in progress

--- src/components/editor/canvas/ZoomControls.tsx
+++ src/components/editor/canvas/ZoomControls.tsx
@@ -1,21 +1,29 @@
 import { PlusIcon, MinusIcon } from '@phosphor-icons/react';
+import ToolTip from '../../common/ToolTip';
 
 export const ZoomControls = () => {
   return (
-    <div>
-      <div>
-        <button>
+    <div className="flex justify-around items-center w-32 h-10 rounded absolute bottom-2 right-4 bg-white">
+      <ToolTip hint="Zoom Out">
+        <button className="btn-sm btn-square flex justify-center items-center rounded cursor-pointer hover:bg-[var(--background-hover)]">
           <MinusIcon />
         </button>
+      </ToolTip>
+      <div className="w-10">
+        <input
+          className="w-full text-center text-sm "
+          type="text"
+          value={'100%'}
+          readOnly
+        />
       </div>
-      <div>
-        <input type="text" readOnly />
-      </div>
-      <div>
-        <button>
+      <ToolTip hint="Zoom In">
+        <button className="btn-sm btn-square flex justify-center items-center rounded cursor-pointer hover:bg-[var(--background-hover)]">
           <PlusIcon />
         </button>
-      </div>
+      </ToolTip>
     </div>
   );
 };
+
+export default ZoomControls;
